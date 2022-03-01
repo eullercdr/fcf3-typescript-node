a8f4fe0b245a918f56f7f2d958d23e2955bafa79
Euller <eullercdr@gmail.com>
2022-03-01 14:51:51 -0300

    feat: define properties category

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,3 +1,26 @@
+export type CategoryProperties = {
+  name: string;
+  description: string;
+  is_active: boolean;
+  created_at: Date;
+};
+
 export class Category {
-  constructor() {}
+  constructor(readonly props: CategoryProperties) {}
+
+  get name(): string {
+    return this.props.name;
+  }
+
+  get description(): string {
+    return this.props.description;
+  }
+
+  get is_active(): boolean {
+    return this.props.is_active;
+  }
+
+  get created_at(): Date {
+    return this.props.created_at;
+  }
 }
